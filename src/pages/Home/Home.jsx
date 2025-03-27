7871d921c638e430f7509f921afd2c79cf24b420
Darkryza <82858316+Darkryza@users.noreply.github.com>
2025-03-28 00:56:35 +0800

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Home.css"
 
 function Home() {
-  return (
+    return (
     <>
         <div className="home-page">
             <div className="home-container">
@@ -35,15 +35,25 @@ function Home() {
                     <form action="">
                         <div className="amount-layer">
                             <label htmlFor="amount">Amount</label>
-                            <input type="number" name='amount'/>
-                            <button className="add-btn">Add</button>
-                            <button className="deduct-btn">Deduct</button>
+                            <div className="input-layer">
+                                <input 
+                                type="number" 
+                                name="amount" 
+                                className="input-amount" 
+                                />
+                                <div className="btn-amount">
+                                    <button className="btn add-btn">Add</button>
+                                    <button className="btn deduct-btn">Deduct</button>
+                                </div>
+                            </div>
                         </div>
                         <div className="desc-layer">
                             <label htmlFor="desc">Description</label>
-                            <input type="text" />
+                            <textarea name="desc" className='input-desc' rows={3}></textarea>
                         </div>
-                        <button className='submit-btn'>Submit</button>
+                        <div className="submit-btn">
+                            <button className='submit-btn'>Submit</button>
+                        </div>
                     </form>
                 </div>
             </div>
